0a2e4f26869b2597910f52f39fcd70a68bce920c
EmIT <emad.armoun@gmail.com>
2021-04-16 16:48:47 +0430

    Add init login page & user context

--- src/App.js
+++ src/App.js
@@ -1,22 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Switch, Route, BrowserRouter } from "react-router-dom";
 import BuyList from './containers/BuyList/BuyList';
+import Login from './containers/Login/Login';
 import { UserContext } from './util/userContext'
 
-const App = props => {
-    const userInfo = {
-        id: 1,
-        userName: 'emit',
-        firstName: 'emad',
-        lastName: 'armoun'
-    };
+const App = () => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [userInfo, setUserInfo] = useState(null)
+
+    const checkLogin = () => {
+        const userInfoResult = {
+            id: 1,
+            userName: 'emit',
+            firstName: 'عماد',
+            lastName: 'آرمون'
+        };
+        setUserInfo(userInfoResult);
+
+        setIsLoggedIn(true);
+    }
+
+    useEffect(() => {
+        checkLogin();
+
+        return () => {
+            //cleanup
+        }
+    }, [])
 
     return (
         <div className='app-wrapper'>
             <BrowserRouter>
                 <Switch>
                     <UserContext.Provider value={userInfo}>
-                        <Route exact path="/" component={BuyList} />
+                        {
+                            isLoggedIn
+                            ? (
+                                <Route exact path="/" component={BuyList} />
+                                // <Route path="/other-path" component={OtherComponent} />
+                            )
+                            : (
+                                <Route exact path="*" component={Login} />
+                            )
+                        }
                     </UserContext.Provider>
                 </Switch>
             </BrowserRouter>
