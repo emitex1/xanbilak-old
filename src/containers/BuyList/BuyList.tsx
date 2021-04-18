002a1793aef64edc29662bb3b67aad5ae118690d
EmIT <emad.armoun@gmail.com>
2021-04-18 21:31:34 +0430

    Add initial buy list

--- src/containers/BuyList/BuyList.tsx
+++ src/containers/BuyList/BuyList.tsx
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 const BuyList = (props: any) => {
+    const [items, setItems] = useState([]);
+    
+    useEffect(() => {
+        setItems([
+            { id: 1, title: "سیب" },
+            { id: 2, title: "دفتر یادداشت" },
+        ]);
+
+        return () => {
+            //cleanup
+        }
+    }, [])
+
     return (
         <div className='page-wrapper'>
             <div className='add-box'>
                 <input type='text' />
             </div>
-            <div className='buy-list'></div>
+            <div className='buy-list'>
+                { items.map( (item:any, index: number) => {
+                    return (
+                        <li key={item.id}>{item.title}</li>
+                    );
+                })}
+            </div>
             {/* <NavBar /> */}
         </div>
     )
