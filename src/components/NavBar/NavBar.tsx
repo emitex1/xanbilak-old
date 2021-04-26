0b9599803f3731bb012585ccae67d73c6b3d7ea5
EmIT <emad.armoun@gmail.com>
2021-04-26 23:18:25 +0430

    Add NavBar

---
+++ src/components/NavBar/NavBar.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './NavBar.scss';
+
+const NavBar = () => {
+    return (
+        <div className='navbar'>
+            <div>
+                لیست خرید
+            </div>
+            <div>
+                پیام رسان
+            </div>
+            <div>
+                مشخصات خانواده
+            </div>
+        </div>
+    )
+}
+
+export default NavBar
--- src/containers/ShoppingList/ShoppingList.tsx
+++ src/containers/ShoppingList/ShoppingList.tsx
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from 'react';
+import NavBar from '../../components/NavBar/NavBar';
 import { BuyableItem } from '../../types/BuyableItem';
 import { BuyableItemsData } from './BuyableItemsData';
 import ShoppingItem from './ShoppingItem';
@@ -54,7 +55,7 @@ const ShoppingList = (props: any) => {
                 )}
                 </ul>
             </div>
-            {/* <NavBar /> */}
+            <NavBar />
         </div>
     )
 }
