3c6d54ad14a1af44287935141ae4e76f8c6f0db8
EmIT <emad.armoun@gmail.com>
2021-05-11 11:33:29 +0430

    Add white logo to top bar

--- src/components/MainHeader/MainHeader.tsx
+++ src/components/MainHeader/MainHeader.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './MainHeader.scss';
+import whiteLogo from '../../assets/images/logo-white.png';
 
 const MainHeader = () => {
     return (
         <div className='main-header'>
-            <span className='title'>Xanbilak</span>
+            <span className='title'>anbilak</span>
+            <img src={whiteLogo} alt="Xanbilak Logo" className='xanbilak-logo' />
         </div>
     )
 }
