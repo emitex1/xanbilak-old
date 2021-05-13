b1770d10dc4e89e09c7a942a24de33783e76b055
EmIT <emad.armoun@gmail.com>
2021-05-14 00:48:47 +0430

    Add sendData function

--- src/util/ApiTools.ts
+++ src/util/ApiTools.ts
@@ -2,7 +2,29 @@ import { apiBaseUrl, apiVersion } from '../settings';
 
 export const getData = (apiAddress: string, baseUrl: string = apiBaseUrl, version: string = apiVersion) => {
     return fetch(baseUrl + '/' + version + '/' + apiAddress, {
-        "method" : "get"
+        "method" : "GET",
+        "headers": {
+            "accept": "application/json"
+        },
+    }).then(
+        response => response.json()
+    )
+    .then( response => {
+        return response;
+    })
+    .catch(err => {
+        console.error(err);
+    });
+}
+
+export const sendData = (apiAddress: string, data: any, baseUrl: string = apiBaseUrl, version: string = apiVersion) => {
+    return fetch(baseUrl + '/' + version + '/' + apiAddress, {
+        "method" : "POST",
+        "headers": {
+            "content-type": "application/json",
+            "accept": "application/json"
+        },
+        "body": JSON.stringify(data)
     }).then(
         response => response.json()
     )
