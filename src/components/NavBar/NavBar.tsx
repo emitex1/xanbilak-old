81e9279f86565c36a61933792751b4c959f3e363
EmIT <emad.armoun@gmail.com>
2021-05-22 00:42:59 +0430

    Add change language function into settings page

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 import { useTranslation } from "react-i18next";
-import i18next from "i18next";
 import './NavBar.scss';
 import { IoMdSettings } from 'react-icons/io';
 import { TiMessages } from 'react-icons/ti';
@@ -9,20 +8,6 @@ import { FaListOl } from 'react-icons/fa';
 
 const NavBar = () => {
     const { t } = useTranslation();
-
-    const changeLanguage = () => {
-        const selected = localStorage.getItem("i18nextLng") || "en";
-        if(selected === "en") {
-            localStorage.setItem("i18nextLng", "fa");
-            i18next.changeLanguage("fa");
-            console.log("fa");
-        }
-        else {
-            localStorage.setItem("i18nextLng", "en");
-            i18next.changeLanguage("en");
-            console.log("en");
-        }
-    }
 
     return (
         <div className='navbar'>
--- src/containers/Settings/Settings.tsx
+++ src/containers/Settings/Settings.tsx
@@ -1,11 +1,37 @@
-import React from 'react'
+import React, {useState} from 'react'
 import NavBar from '../../components/NavBar/NavBar'
+import i18next from "i18next";
 
 const Settings = () => {
+    const [lang, setLang] = useState("فارسی");
+
+    const changeLanguage = () => {
+        const selected = localStorage.getItem("i18nextLng") || "en";
+        if(selected === "en") {
+            localStorage.setItem("i18nextLng", "fa");
+            i18next.changeLanguage("fa");
+            console.log("fa");
+            setLang("فارسی");
+        }
+        else {
+            localStorage.setItem("i18nextLng", "en");
+            i18next.changeLanguage("en");
+            console.log("en");
+            setLang("English");
+        }
+    }
+
     return (
         <div>
             <center>
                 <h2>Settings</h2>
+                <hr />
+
+                <span>Language / زبان :</span>
+                <br />
+                <button onClick={changeLanguage}>
+                    {lang}
+                </button>
             </center>
             <NavBar />
         </div>
