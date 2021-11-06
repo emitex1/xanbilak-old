7ce9cf9f0e5cff7ef77005dea628d6151c81c363
EmIT <emad.armoun@gmail.com>
2021-11-07 01:22:04 +0330

    chore: manage style loaders

---
+++ jest.conf.js
@@ -0,0 +1,7 @@
+/* eslint-disable no-undef */
+module.exports = {
+  "moduleNameMapper": {
+    // "^.+\\.(css|less|scss)$": "identity-obj-proxy",
+    "^.+\\.(css|less|scss)$": "babel-jest",
+  },
+};
